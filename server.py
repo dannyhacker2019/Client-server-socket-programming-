e154880d5745c1230a48a003f041c288b5c5958c
danny <dannyhacker2019@gmail.com>
2023-03-21 12:17:46 +0100

    TTT

---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+
+s = socket.socket()
+print('Socket Created')
+
+s.bind(('localhost',9999))
+
+s.listen(3)
+print('Waiting for una connections')
+
+while True:
+    c, addr = s.accept()
+    name = c.recv(1024).decode()
+    print("Connected with ", addr, name)
+
+    c.send(bytes('Welcome to Dannyk Inc','utf-8'))
+
+    c.close()
